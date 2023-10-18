d9402fbf5988728f1dca040036ee08e772b66265
kkk30 <yarinige30@gmail.com>
2023-10-18 19:27:31 +0900

    test1

--- SamplePlugin/Plugin.cs
+++ SamplePlugin/Plugin.cs
@@ -1,79 +1,84 @@
-﻿using Dalamud.Game.Command;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using Dalamud.Game;
+using Dalamud.Game.ClientState.Objects;
+using Dalamud.Game.ClientState.Objects.Types;
+using Dalamud.Game.Command;
+using Dalamud.Game.Gui;
 using Dalamud.IoC;
+using Dalamud.Logging;
 using Dalamud.Plugin;
-using System.IO;
-using Dalamud.Interface.Windowing;
 using Dalamud.Plugin.Services;
-using SamplePlugin.Windows;
+using FFXIVClientStructs.FFXIV.Client.Game.Character;
+using FFXIVClientStructs.FFXIV.Client.UI;
+using BattleChara = Dalamud.Game.ClientState.Objects.Types.BattleChara;
+using Character = Dalamud.Game.ClientState.Objects.Types.Character;
 
 namespace SamplePlugin
 {
-    public sealed class Plugin : IDalamudPlugin
-    {
-        public string Name => "Sample Plugin";
-        private const string CommandName = "/pmycommand";
+	public class Plugin : IDalamudPlugin
+	{
+		[PluginService] public static DalamudPluginInterface DalamudPluginInterface { get; private set; }
+		[PluginService] public static ITargetManager TargetManager { get; private set; }
+		//[PluginService] public static ObjectTable ObjectTable { get; private set; }
+		[PluginService] public static IGameGui GameGui { get; private set; }
+		[PluginService] public static ICommandManager CommandManager { get; private set; }
+		[PluginService] public static SigScanner SigScanner { get; private set; }
+        [PluginService] public static IPluginLog PluginLog { get; private set; }
 
-        private DalamudPluginInterface PluginInterface { get; init; }
-        private ICommandManager CommandManager { get; init; }
-        public Configuration Configuration { get; init; }
-        public WindowSystem WindowSystem = new("SamplePlugin");
+        private delegate long ExecuteCommandDele(int id, int a1, int a2, int a3, int a4);
+		private ExecuteCommandDele ExecuteCommand;
 
-        private ConfigWindow ConfigWindow { get; init; }
-        private MainWindow MainWindow { get; init; }
+		public void Dispose()
+		{
+			CommandManager.RemoveHandler("/resetenmity");
+			CommandManager.RemoveHandler("/resetenmityall");
+		}
 
-        public Plugin(
-            [RequiredVersion("1.0")] DalamudPluginInterface pluginInterface,
-            [RequiredVersion("1.0")] ICommandManager commandManager)
-        {
-            this.PluginInterface = pluginInterface;
-            this.CommandManager = commandManager;
+		public unsafe Plugin()
+		{
+			var scanText = SigScanner.ScanText("E8 ?? ?? ?? ?? 8D 43 0A");
+			ExecuteCommand = Marshal.GetDelegateForFunctionPointer<ExecuteCommandDele>(scanText);
+            PluginLog.Debug($"{nameof(ExecuteCommand)} +{(long)scanText - (long)Process.GetCurrentProcess().MainModule.BaseAddress:X} ");
 
-            this.Configuration = this.PluginInterface.GetPluginConfig() as Configuration ?? new Configuration();
-            this.Configuration.Initialize(this.PluginInterface);
+			CommandManager.AddHandler("/resetenmity", new CommandInfo(ResetTarget) { HelpMessage = "Reset target dummy's enmity." });
+			CommandManager.AddHandler("/resetenmityall", new CommandInfo(ResetAll) { HelpMessage = "Reset the enmity of all dummies." });
 
-            // you might normally want to embed resources and load them from the manifest stream
-            var imagePath = Path.Combine(PluginInterface.AssemblyLocation.Directory?.FullName!, "goat.png");
-            var goatImage = this.PluginInterface.UiBuilder.LoadImage(imagePath);
+			void ResetEnmity(int objectId)
+			{
+                PluginLog.Information($"resetting enmity {objectId:X}");
+				ExecuteCommand(0x140, objectId, 0, 0, 0);
+			}
 
-            ConfigWindow = new ConfigWindow(this);
-            MainWindow = new MainWindow(this, goatImage);
-            
-            WindowSystem.AddWindow(ConfigWindow);
-            WindowSystem.AddWindow(MainWindow);
+			void ResetTarget(string s, string s1)
+			{
+				var target = TargetManager.Target;
+				if (target is Character { NameId: 541 }) ResetEnmity((int)target.ObjectId);
+			}
 
-            this.CommandManager.AddHandler(CommandName, new CommandInfo(OnCommand)
-            {
-                HelpMessage = "A useful message to display in /xlhelp"
-            });
+			void ResetAll(string s, string s1)
+			{
+				var addonByName = GameGui.GetAddonByName("_EnemyList", 1);
+				if (addonByName != IntPtr.Zero)
+				{
+					var addon = (AddonEnemyList*)addonByName;
+					var numArray = FFXIVClientStructs.FFXIV.Client.System.Framework.Framework.Instance()->GetUiModule()->GetRaptureAtkModule()->AtkModule.AtkArrayDataHolder.NumberArrays[19];
 
-            this.PluginInterface.UiBuilder.Draw += DrawUI;
-            this.PluginInterface.UiBuilder.OpenConfigUi += DrawConfigUI;
-        }
-
-        public void Dispose()
-        {
-            this.WindowSystem.RemoveAllWindows();
-            
-            ConfigWindow.Dispose();
-            MainWindow.Dispose();
-            
-            this.CommandManager.RemoveHandler(CommandName);
-        }
-
-        private void OnCommand(string command, string args)
-        {
-            // in response to the slash command, just display our main ui
-            MainWindow.IsOpen = true;
-        }
-
-        private void DrawUI()
-        {
-            this.WindowSystem.Draw();
-        }
-
-        public void DrawConfigUI()
-        {
-            ConfigWindow.IsOpen = true;
-        }
-    }
+					for (var i = 0; i < addon->EnemyCount; i++)
+					{
+						var enemyObjectId = numArray->IntArray[8 + i * 6];
+						var enemyChara = CharacterManager.Instance()->LookupBattleCharaByObjectId((uint)enemyObjectId);
+						if (enemyChara is null) continue;
+						if (enemyChara->Character.NameID == 541) ResetEnmity(enemyObjectId);
+					}
+				}
+			}
+		}
+		public static string Name => "Sample Plugin";
+	}
 }
